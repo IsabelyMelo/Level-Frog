67abcd782f2fb66153611b1150f3dde782bb6e40
Isabely Melo <isabelymelo09@gmail.com>
2025-11-26 00:23:39 -0300

    Adds levels 4 and 5 to level select

--- novo-projeto-jogo/scenes/menus/levels_map.gd
+++ novo-projeto-jogo/scenes/menus/levels_map.gd
@@ -4,7 +4,11 @@ func _ready() -> void:
 	for botao in get_children():
 		if botao.name.begins_with("level_"):
 			botao.modulate.a = 0.0 
+			
 
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+	
 func _on_level_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level-1.tscn")
 
@@ -14,5 +18,8 @@ func _on_level_2_pressed() -> void:
 func _on_level_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level-3.tscn")
 
-func _on_exit_pressed() -> void:
-	get_tree().quit()
+func _on_level_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level-4.tscn")
+
+func _on_level_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/level-5.tscn")
